2527a1de77622cca0d215d4075b7110c4a065fd5
satyamsacham01 <sachansatyam19@gmail.com>
2025-04-13 00:59:42 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -33,6 +33,6 @@ app.use("/api/user",express.json(),userRouter)
 // Port
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port $(PORT)`)
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
 })
